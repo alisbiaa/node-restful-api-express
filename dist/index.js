077dd3496125e060f6d47e08828c7e451afa0fa4
alisbiaa <cn4f59@inf.elte.hu>
2021-06-03 19:02:23 +0200

    Basic setup

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works !'
+    });
+});
+module.exports = app;
+//# sourceMappingURL=index.js.map
